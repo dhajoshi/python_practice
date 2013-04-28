87baa1164c2dfe9f55fc491128fc92cc56a1ef3e
Dhaval Joshi <dhajoshi@dhalinux001.example.com>
2013-04-28 11:17:55 +0530

    First Commit

---
+++ ex2.py
@@ -0,0 +1,25 @@
+
+print "I Will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "now i will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 +2 < 5 -7?"
+
+print 3 + 2 < 5 - 7
+
+print "what is 3 + 2?", 3+ 2
+print "what is 5 - 7?", 5 - 7
+
+print "oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it Greater?", 5>-2
+print "Is i greater or equal?", 5>=-2
+print "is it less or equal?", 5<=-2
+
---
+++ ex4.py
@@ -0,0 +1,17 @@
+
+my_name = 'Dhaval Joshi'
+my_age = 28
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = "brown"
+my_teeth = "White"
+my_hair = "Black"
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "his teeth are usually %s depending on the coffee." %my_teeth
+
+print "if i add %d, %d, and %d i get %d." % (my_age, my_height, my_weight, my_age+my_height+my_weight)
